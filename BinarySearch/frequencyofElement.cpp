ab94c88a6e056d82cb91fc41ebf338ff24f026e2
shirsho-roy <shirsho.tffs@gmail.com>
2023-06-14 01:27:17 +0530

    Updates

---
+++ BinarySearch/frequencyofElement.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+int firstoccurence(int arr[],int n,int k){
+    int res=-1;
+    int s=0;
+    int e=n-1;
+    while(s<=e){
+        int mid=s+(e-s)/2;
+        if(arr[mid]==k) {
+            res=mid;
+            e=mid-1;//Now check before this occurence for 1st occurence
+        }
+        else if(arr[mid]<k) s=mid+1;
+        else e=mid-1;
+    }
+   return res;
+}
+
+int lastoccurence(int arr[],int n,int k){
+    int res=-1;
+    int s=0;
+    int e=n-1;
+    while(s<=e){
+        int mid=s+(e-s)/2;
+        if(arr[mid]==k) {
+            res=mid;
+            s=mid+1;//Now check after this occurence for last occurence
+        }
+        else if(arr[mid]<k) s=mid+1;
+        else e=mid-1;
+    }
+   return res;
+}
+
+void frequency(int arr[],int n,int k){
+  //Function returns frequency of k in arr
+
+  int f=firstoccurence(arr,n,k);
+  int l=lastoccurence(arr,n,k);
+  int freq=l-f+1;
+  cout<<"Frequency of"<<k<<"is:"<<freq;
+}
